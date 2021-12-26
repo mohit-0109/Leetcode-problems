256ced9f03379d01ed63cf3774e65b2aa61cfa83
mohit-0109 <mohit.nandargi@gmail.com>
2021-12-26 09:18:55 +0530

    If a tree is a symmetric tree problem

---
+++ Symmetric tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool check(TreeNode* left,TreeNode* right)
+    {
+        if(!left && !right)
+            return true;
+        if(!left || !right)
+            return false;
+        if(left->val != right->val)
+            return false;
+        return check(left->right,right->left) && check(left->left,right->right);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL )
+            return false;
+        return check(root->left,root->right);
+    }
+};
