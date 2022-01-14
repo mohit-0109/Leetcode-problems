c2f119c0a5ef8816863a2aa8b0a69c4b3377064e
mohit-0109 <mohit.nandargi@gmail.com>
2022-01-14 12:57:53 +0530

    shortest path dijkstarsalgo

---
+++ dijkstras algorithm to find shortest path.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+# define INF 0x3f3f3f3f 
+  
+// This class represents a directed graph using  
+// adjacency list representation 
+class Graph 
+{ 
+    int V;    // No. of vertices 
+  
+    // In a weighted graph, we need to store vertex  
+    // and weight pair for every edge 
+    list< pair<int, int> > *adj; 
+  
+public: 
+    Graph(int V);  // Constructor 
+  
+    // function to add an edge to graph 
+    void addEdge(int u, int v, int w); 
+  
+    // prints shortest path from s 
+    void shortestPath(int s); 
+}; 
+  
+// Allocates memory for adjacency list 
+Graph::Graph(int V) 
+{ 
+    this->V = V; 
+    adj = new list< pair<int, int> >[V]; 
+} 
+  
+void Graph::addEdge(int u, int v, int w) 
+{ 
+    adj[u].push_back(make_pair(v, w)); 
+    adj[v].push_back(make_pair(u, w)); 
+} 
+  
+// Prints shortest paths from src to all other vertices 
+void Graph::shortestPath(int src) 
+{ 
+    set<pair<int,int>> setds;  //weight first, vertex seciond;
+    vector<int> dist(V,INF);
+  
+    setds.insert(make_pair(0,src));
+    dist[src]=0;
+
+    while(!setds.empty())
+    {
+        pair<int,int> temp = *(setds.begin());
+        setds.erase(setds.begin());
+        
+        int u = temp.second;
+
+        for(auto x: adj[u])
+        {
+            int v=x.first;
+            int weight = x.second;
+
+            if( dist[u]+weight < dist[v] )
+            {
+                if(dist[v]!=INF)
+                    setds.erase(setds.find(make_pair(dist[v],v)));
+                dist[v]= dist[u]+weight;
+                setds.insert(make_pair(dist[v],v));
+            }
+        }
+
+    }
+
+    // Print shortest distances stored in dist[] 
+    printf("Vertex   Distance from Source\n"); 
+    for (int i = 0; i < V; ++i) 
+        printf("%d \t\t %d\n", i, dist[i]); 
+} 
+  
+// Driver program to test methods of graph class 
+int main() 
+{ 
+    // create the graph given in above fugure 
+    int V = 9; 
+    Graph g(V); 
+  
+    //    making above shown graph 
+    g.addEdge(0, 1, 4); 
+    g.addEdge(0, 7, 8); 
+    g.addEdge(1, 2, 8); 
+    g.addEdge(1, 7, 11); 
+    g.addEdge(2, 3, 7); 
+    g.addEdge(2, 8, 2); 
+    g.addEdge(2, 5, 4); 
+    g.addEdge(3, 4, 9); 
+    g.addEdge(3, 5, 14); 
+    g.addEdge(4, 5, 10); 
+    g.addEdge(5, 6, 2); 
+    g.addEdge(6, 7, 1); 
+    g.addEdge(6, 8, 6); 
+    g.addEdge(7, 8, 7); 
+  
+    g.shortestPath(0); 
+  
+    return 0; 
+} 
